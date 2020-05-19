6ccbecebd0f2eda9b8cbeaf9964a948694a536ce
Ali Turşucular <alitursucular@gmail.com>
2020-05-19 06:13:41 +0100

    Updates

---
+++ precache-manifest.d8e2805afd7a0668cb40077376cd2159.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa386f30bd237191189e11f7b1afe4e",
+    "url": "/names-list-with-react-and-typescript-demo/index.html"
+  },
+  {
+    "revision": "dad8bcebde8af116b534",
+    "url": "/names-list-with-react-and-typescript-demo/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8c91814aa64f920e4957",
+    "url": "/names-list-with-react-and-typescript-demo/static/css/main.9feb3e4e.chunk.css"
+  },
+  {
+    "revision": "dad8bcebde8af116b534",
+    "url": "/names-list-with-react-and-typescript-demo/static/js/2.4ba885c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/names-list-with-react-and-typescript-demo/static/js/2.4ba885c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c91814aa64f920e4957",
+    "url": "/names-list-with-react-and-typescript-demo/static/js/main.c352c450.chunk.js"
+  },
+  {
+    "revision": "7bfe01d49f78ccb20565",
+    "url": "/names-list-with-react-and-typescript-demo/static/js/runtime-main.76efb74b.js"
+  },
+  {
+    "revision": "eb2df43a0add3db8bce9374058f8c82f",
+    "url": "/names-list-with-react-and-typescript-demo/static/media/typescript-logo-512.eb2df43a.png"
+  }
+]);
