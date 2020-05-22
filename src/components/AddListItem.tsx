0678b29d3fc4194022f114743b2e312f156f95a9
Ali Turşucular <alitursucular@gmail.com>
2020-05-22 16:46:55 +0100

    Button icon update

--- src/components/AddListItem.tsx
+++ src/components/AddListItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { faLevelDownAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface AddListItemProps {
@@ -25,7 +25,7 @@ export const AddListItem: React.FC<AddListItemProps> = ({ addName }) => {
                 <form className="app_list_form" onSubmit={handleSubmit}>
                     <input className="app_list_form--input" type="text" value={newName} onChange={handleChange} placeholder="Please type a name..." autoFocus />
                     <button className="app_list_form--button" type="submit" >
-                        <FontAwesomeIcon icon={faPlusCircle} />
+                        <FontAwesomeIcon icon={faLevelDownAlt} rotation={90} />
                     </button>
                 </form>
             </div>
